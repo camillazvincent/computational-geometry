c6143c51f6c334e921b8a813096cc9323f44c2ae
jerrystamm <jaredstamm00@gmail.com>
2024-02-17 16:59:20 -0500

    Updated closest_pair.py
    Added plot visualization and user input

--- closest_pair.py
+++ closest_pair.py
@@ -1,5 +1,6 @@
 import math
 from geometry import Point
+import matplotlib.pyplot as plt
 
 # References:
 # https://www.cs.cmu.edu/~ckingsf/bioinfo-lectures/closepoints.pdf
@@ -89,3 +90,57 @@ def closest_pair(points):
     y_sorted = sorted(points, key=lambda point: point.y)
 
     return optimized_pair(x_sorted, y_sorted)
+
+
+def plot_points_and_line(points, closest_pair):
+
+    """
+        Function to plot all points and the closest pair.
+
+        Args:
+            points: a set of points
+            closest_pair: points that are the closest pair
+            
+    """
+
+    x = [point.x for point in points]
+    y = [point.y for point in points]
+
+    plt.scatter(x, y, label='Points')
+
+    line_x = [closest_pair[1][0].coordinates[0], closest_pair[1][1].coordinates[0]]
+    line_y = [closest_pair[1][0].coordinates[1], closest_pair[1][1].coordinates[1]]
+
+    plt.plot(line_x, line_y, label='Closest Pair')
+
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Scatter Plot of Points')
+    plt.legend()
+    plt.show()
+
+
+points = []
+
+print("Enter coordinate point in the format 'x y'. For example, '1 2'. Type 'E' to end.")
+
+while True:
+    user_input = input()
+
+    if user_input.upper() == 'E':
+        break
+
+    try:
+        x, y = map(float, user_input.split())
+        points.append(Point(x, y))
+    except ValueError:
+        print("Invalid input. Please enter numbers.")
+        continue
+
+result = closest_pair(points)
+
+print(f"First pair: {result[1][0]}")
+print(f"Second pair: {result[1][1]}")
+print(f"Distance between: {result[0]}")
+
+plot_points_and_line(points, result)
