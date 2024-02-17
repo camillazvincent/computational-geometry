14eef1e9de0a0522d6b3dddb368f7b39e07b39e9
Camilla Vincent <camillazvincent@gmail.com>
2024-02-17 15:14:14 -0500

    fix errors in main.py again

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
         selection = input("Please select what you would like to do. \n 1. Intersection \n 2. Closest pair \n 3. Convex hull \n 4. Largest Circle \n")
 
         if selection.startswith('1') or selection.lower().startswith('i'):
-            runpy.run_module('intersection.py')
+            runpy.run_module('intersection')
             break
         elif selection.startswith('2') or selection.lower().startswith('cl'):
             break
@@ -13,7 +13,7 @@ def main():
             runpy.run_module('convex_hull')
             break
         elif selection.startswith('4') or selection.lower().startswith('l'):
-            runpy.run_module('largest_circle.py')
+            runpy.run_module('largest_circle')
             break
         else:
             print("Invalid input. Please enter a number between 1 and 4.")
